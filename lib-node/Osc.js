daaa5daf0cd64007d38bc442fd5971f50c048576
hoch <hongchan.choi@gmail.com>
2012-07-13 20:59:09 -0700

    lib-node: added Osc.js, cleaned up PolyBlitX.js

---
+++ lib-node/Osc.js
@@ -0,0 +1,103 @@
+// ========================================================================
+// WAAX > NODE
+// Osc.js
+// 
+// a part of WAAX.js
+// - Web Audio API eXtension for Chrome/Safari
+// - revision: 1
+// 
+//                                 written by 
+//             Hongchan Choi           |           Juhan Nam 
+//      hongchan@ccrma.stanford.edu    |    juhan@ccrma.stanford.edu
+// ========================================================================
+
+/* Copyright (C) 2011-2012  Juhan Nam & Hongchan Choi
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+
+// ------------------------------------------------------------------------
+// class - SinOsc
+// : Basic Sinewave Oscillator
+// 
+// @author Hongchan Choi / hongchan@ccrma.stanford.edu
+// ------------------------------------------------------------------------
+
+// variables ..............................................................
+WAAX.Node.SinOsc = function(_AudioContext)
+{  
+    // version
+    this.version = 1;
+
+    // synth params
+    this.freq = 261.626; // middle C
+    this.gain = 1.0;
+    this.phase = 0.0;
+    this.factor = WAAX.TWOPI / WAAX.sample_rate;
+    this.step = this.factor * this.freq;
+
+    // api params
+    this.num_input = 1; // for frequency modulation
+    this.num_output = 2;
+    this.context = _AudioContext;
+    this.node = _AudioContext.createJavaScriptNode(
+	WAAX.buffer_size, 
+	this.num_input, 
+	this.num_output);
+    var ref = this;
+    this.node.onaudioprocess = function(e) { ref.cb(e); };
+};
+
+
+// prototypes ..............................................................
+WAAX.Node.SinOsc.prototype = {
+   
+    // constructor
+    constructor: WAAX.Node.SinOsc,
+    
+    // connect & disconnect
+    connect: function(_output_node) { this.node.connect(_output_node); },
+    disconnect: function() { this.node.disconnect(); },
+
+    // setFreq
+    setFreq: function(_freq) { this.freq = _freq; },
+
+    // setGain
+    setGain: function(_gain) { this.gain = _gain; },
+
+    // setParams
+    setParams: function(_freq, _gain) { 
+	this.setFreq(_freq);
+	this.setGain(_gain);
+    },
+    
+    // callback
+    cb: function(_e) {
+	// get references to callback buffers
+	var inL = _e.inputBuffer.getChannelData(0); // mono input
+	var bufL = _e.outputBuffer.getChannelData(0);
+	var bufR = _e.outputBuffer.getChannelData(1);
+	// filling up sample buffer
+	for (var i = 0; i < WAAX.buffer_size; ++i) {
+	    this.step = this.factor * ( this.freq + inL[i]);
+	    this.phase = (this.phase + this.step ) % WAAX.TWOPI;
+	    var sample = Math.sin(this.phase);
+	    bufL[i] = bufR[i] = sample * this.gain;
+	}
+    }
+};
+
+// END OF FILE
