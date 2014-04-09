60989974b64f9bcd924dc31607421e7a5edbc1cb
Colin Sullivan <colin@colin-sullivan.net>
2014-04-09 13:14:52 -0700

    Refactored `Sampler` to support `onended` and multiple calls to `stop`.
    This implementation is not meant to be a polyphonic sampler, although it
    could evolve into one.  I have implemented a list of active nodes instead of
    just keeping track of the most recent.  This list is maintained so the nodes
    have time to finish and call `onended` before they are removed from the heap.
    Since they aren't removed immediately after calling `stop`, it allows `stop`
    to be called multiple times, because we wait until the sound actually finishes
    to remove the reference to the node.
    
    Calling stop multiple times is specified here:
    
    http://webaudio.github.io/web-audio-api/#widl-AudioBufferSourceNode-stop-void-double-when

--- test/test.sampler.js
+++ test/test.sampler.js
@@ -28,23 +28,32 @@ describe("Sampler", function(){
     });
 
     it("should have played for 4 seconds", function (done) {
-      this.timeout(6500);
-      setTimeout(done, 6000);
+      this.timeout(6000);
+      sampler.onended = function () {
+        done();
+      };
     });
   });
 
   describe("manual stop", function() {
     var playDuration = 2.0,
       startDelay = 3.0;
-    it("should start playing in " + startDelay + " seconds then stop playing " + playDuration + " seconds later", function () {
+    it("should start playing in " + startDelay +
+       " seconds then stop playing " + playDuration + " seconds later",
+    function () {
       var startTime = WX.now + startDelay;
       sampler.start(startTime);
       sampler.stop(startTime + playDuration);
     });
-    it("should have played for " + playDuration + " seconds", function (done) {
-      this.timeout((startDelay + playDuration) * 1000 + 500);
-      setTimeout(done, (startDelay + playDuration) * 1000);
-    });
+    it(
+      "should have played for " + playDuration + " seconds",
+      function (done) {
+        this.timeout((startDelay + playDuration) * 1000);
+        sampler.onended = function () {
+          done();
+        };
+      }
+    );
   });
 
   describe("reschedule stop", function() {
@@ -52,23 +61,41 @@ describe("Sampler", function(){
       rescheduledPlayDuration = 4.0,
       startDelay = 3.0,
       startTime;
-    it("should start playing in " + startDelay + " seconds then stop playing " + playDuration + " seconds later", function () {
-      startTime = WX.now + startDelay;
-      sampler.start(startTime);
-      sampler.stop(startTime + playDuration);
+    it(
+      "should start playing in " + startDelay +
+      " seconds then stop playing " + playDuration +
+      " seconds later",
+      function () {
+        startTime = WX.now + startDelay;
+        sampler.start(startTime);
+        sampler.stop(startTime + playDuration);
+      }
+    );
+    it("should have started playing", function (done) {
+      this.timeout(startDelay * 1000 + 250);
+      setTimeout(done, (startDelay * 1000));
     });
-    it("should reschedule stop time to sooner, " + rescheduledPlayDuration + " seconds", function (done) {
-      this.timeout(3000);
-      setTimeout(function () {
-        sampler.stop(startTime + rescheduledPlayDuration);
-        done();
-      }, 2500);
-    });
-    it("should have played for " + rescheduledPlayDuration + " seconds", function (done) {
-      this.timeout((startDelay + rescheduledPlayDuration) * 1000 + 500);
-      setTimeout(done, (startDelay + rescheduledPlayDuration) * 1000);
-    });
-    
+    it(
+      "should reschedule stop time to sooner, " + rescheduledPlayDuration +
+      " seconds",
+      function (done) {
+        this.timeout(1250);
+        setTimeout(function () {
+          sampler.stop(startTime + rescheduledPlayDuration);
+          done();
+        }, 1000);
+      }
+    );
+    it(
+      "should have played for " + rescheduledPlayDuration + " seconds",
+      function (done) {
+        this.timeout((startDelay + rescheduledPlayDuration) * 1000 + 1000);
+        sampler.onended = function () {
+          (startTime + rescheduledPlayDuration).should.be.approximately(WX.now, 0.01);
+          done();
+        };
+      }
+    );
   });
 
 });
