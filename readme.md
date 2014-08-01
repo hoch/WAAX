76021ec009e8938f59dda7a0f8c7d8f52e2a2f2b
Hongchan Choi <hoch@dn0a23286e.sunet>
2014-07-31 17:06:36 -0700

    timebase finished, test needs more work. rewriting readme

--- readme.md
+++ readme.md
@@ -1,29 +1,70 @@
 # WAAX: Web Audio API eXtension
 
-> NOTE: Currently the repo is under the active developement. Please advise and use it at your own risk.
+Framework for Web-based Music Apps
+
+> Currently the repository is under the active development as alpha status. It has passed the initial experimental implementation and is stabilizing to version 1.0.0. With that said, please use at your own risk.
 
 <!-- travis build image -->
 
-[In-browser Test](#)
-
-### Example
-
-```javascript
-var osc = WX.Osc3({ osc1type: 'square', osc2detune: 700 }),
-    afx = WX.AwesomeEffect();
-osc.to(afx).to(WX.Master);   
-
-var env = WX.Envelope([0.0, 0.0], [1.0, 0.01, 1], [0.0, 0.5, 2]);
-osc.set('osc2type', 'triangle');
-osc.set('freq', WX.mtof(60)).set('gain', env(WX.now + 0.5));
-```
-
+[In-browser Test (v1.0.0-alpha)](#)
 
 # Introduction
-__WAAX__ is a JavaScript library that offers a comprehensive framework for web-based music application. Its goal is to facilitate music and audio programming on the modern web browser. The WAAX core library features:
+__WAAX__ is a JavaScript library that offers a comprehensive framework for web-based music application. Its goal is to facilitate music and audio programming on the modern web browser. The __WAAX__ features:
+
+- Plug-in Interface
+- Parameter & Preset Management
+- Timebase, Transport and NoteList (a.k.a. Sequencer Engine)
+- Modular GUI elements powered by Polymer
+- Web MIDI API Integration _(supported behind flag)_
+- Complete Development setup: NPM, Bower, Grunt
+
+
+# [Play WAAX!](http://playwaax.appspot.com)
+
+You can find playable demos and examples at [PlayWAAX](http://playwaax.appspot.com).
+
+# Basic Usage
+
+
+# Development: Installation
+
+The complete WAAX development setup requires [NPM](http://nodejs.org/), [Bower](http://bower.io/) and [Grunt](http://gruntjs.com/). If you have them installed, then simply execute the following commands in the terminal to install and configure WAAX. Make sure to substitute `YOUR_DIRECTORY` to your installation directory.
+
+~~~bash
+git clone https://github.com/hoch/WAAX YOUR_DIRECTORY
+cd YOUR_DIRECTORY
+make
+~~~
+
+
+# Getting Started: Development
+
+The following grunt command will start development server, which can be accessed at [http://localhost:8000](http://localhost:8000)
+
+~~~bash
+grunt dev
+~~~
+
+# Documentation
+
+# What's Next?
+
+
+## License and Contact
+
+MIT License. Copyright (c) 2014 [Hongchan Choi](https://ccrma.stanford.edu/~hongchan)
+
+
+---
+
+
+
+
+
+The introduction with more detail can be found [here](https://ccrma.stanford.edu/~hongchan/posts/thoughts-behind-rebirth-of-waax/).
 
 ### Plug-in interface
-WAAX proposes a draft of __audio plug-in format__ for web-based music application, such as [VST, RTAS and AudioUnit](http://en.wikipedia.org/wiki/Audio_plug-in). With this framework, developers can solely focus on designing instruments, sound effect and visualizer units. More info on creating a WAAX plug-in, see [here](#).
+WAAX version 1 proposes a draft of __audio plug-in format__ for web-based music application, such as [VST, RTAS and AudioUnit](http://en.wikipedia.org/wiki/Audio_plug-in). With this framework, developers can solely focus on designing instruments, sound effect and visualizer units. More info on creating a WAAX plug-in, see [here](https://ccrma.stanford.edu/~hongchan/posts/creating-waax-plugin/).
 
 ```javascript
 var osc = WX.Osc3(),
@@ -33,19 +74,21 @@ var osc = WX.Osc3(),
 osc.to(afx).to(WX.Master);              
 ```
 
+
 ### Parameter & Preset Management
-Web Audio API's `AudioParam` is a hassle for developers without computer music background. A systemic wrapper for parameter control is provided users or plug-in designers with succint syntax and flexiblity. Plus, saving and loading plug-in setting as a preset only gets easier as it should.
+Web Audio API's `AudioParam` is a hassle for developers without computer music background. A systemic wrapper for parameter control is provided users or plug-in designers with succinct syntax and flexibility. Plus, saving and loading plug-in setting as a preset only gets easier as it should.
 
 ```javascript
 // freq changes to MIDI pitch 60 immediately
 osc.set('freq', WX.mtof(60));
 
-// gain ramps up linear and ramps down expoentially with an envelope
+// gain ramps up linear and ramps down exponentially with an envelope
 osc.set('gain', [0.0, 0.0], [1.0, 0.01, 1], [0.0, 0.5, 2]);
 
 // returns a preset object (a collection of parameters)
 osc.getPreset();
 ```
+
 
 ### Polymer-ready: Modular GUI elements
 The current version of WAAX is completely redesigned with [Polymer](http://www.polymer-project.org/) (or Web Component) in mind. With the help of __MUI(Musical User Interface)__, A developer can easily design a polymer GUI element and bind it to WAAX plug-in parameters. 
@@ -55,6 +98,7 @@ Two-way binding between a parameter and a GUI element not only saves you from th
 ```javascript
 MUI.$('knobFreq').bind(osc, 'freq');    // #knobFreq connects to freq
 ```
+
 
 ### Planned: Web MIDI API integration
 Since Web MIDI API is only supported in Chrome Canary behind the flag at the time of writing, the integration of [__Ktrl__](https://github.com/hoch/Ktrl) library into WAAX is not considered yet. In the meantime, __Ktrl__ should be loaded and used separately.
@@ -66,7 +110,7 @@ To use WAAX in a web page, simply load the `waax.js` at the end of `<body>`. Thi
 
 ```html
 <!-- load WAAX and Platform/Polymer -->
-<script src="build/waax.min-0.0.1.js"></script>
+<script src="build/waax.min.js"></script>
 <script src="bower_component/platform.js"></script>
 <!-- load WAAX plug-ins and MUI elements -->
 <script src="build/plugins/awesome-synth.js"></script>
--- src/timebase.js
+++ src/timebase.js
@@ -122,6 +122,7 @@ var Timebase = (function (WX) {
       // when list is empty
       if (this.head === null) {
         this.head = note;
+        this.playhead = this.head;
         this.size++;
         return;
       }
@@ -198,7 +199,7 @@ var Timebase = (function (WX) {
         }
         curr = curr.next;
       }
-      return bucket;
+      return (bucket.length > 0) ? bucket : 0;
     },
 
     getSize: function () {
@@ -213,10 +214,14 @@ var Timebase = (function (WX) {
         curr = curr.next;
       }
       // disconnect all links
-      for (var i = 0; i < bucket.length; i++) {
-        bucket[i].next = null;
+      if (bucket.length > 0) {
+        for (var i = 0; i < bucket.length; i++) {
+          bucket[i].next = null;
+        }
+        return bucket;
+      } else {
+        return null;
       }
-      return bucket;
     },
 
     iterate: function (fn) {
@@ -258,31 +263,30 @@ var Timebase = (function (WX) {
       return null;
     },
 
-    // step: function () {
-    //   if (this.playhead) {
-    //     this.playhead = this.playhead.next;
-    //   }
-    // },
+    rewind: function () {
+      this.playhead = this.head;
+    },
 
-    // rewind: function () {
-    //   this.playhead = this.head;
-    // },
+    setPlayheadAtTick: function (tick) {
+      this.playhead = this.head;
+      // find a note
+      if (this.playhead) {
+        while (this.playhead.start < tick) {
+          this.playhead = this.playhead.next;
+        }
+      }
+    },
 
-    // setPlayheadAtTick: function (tick) {
-    //   var curr = this.head;
-    //   // find a note
-    //   while (curr) {
-    //     if (curr < tick) {
-    //       curr = curr.next;
-    //     } else {
-    //       this.playhead = curr;
-    //       return;
-    //     }
-    //   }
-    //   // deadend
-    //   this.playback = null;
-    //   return;
-    // },
+    scan: function (end) {
+      if (this.playhead) {
+        var bucket = [];
+        while (this.playhead.start < end) {
+          bucket.push(this.playhead);
+          this.playhead = this.playhead.next;
+        }
+        return (bucket.length > 0) ? bucket : null;
+      }
+    },
 
     // TODO:
     // load(json) : json array into linked list
@@ -307,44 +311,44 @@ var Timebase = (function (WX) {
    * @description comparable data structure based on JS native array
    */
 
-  function NoteList_Arr() {
-    this.data = [];
-  }
+  // function NoteList_Arr() {
+  //   this.data = [];
+  // }
 
-  NoteList_Arr.prototype = {
+  // NoteList_Arr.prototype = {
 
-    _cmp: function (a, b) {
-      return a - b;
-    },
+  //   _cmp: function (a, b) {
+  //     return a - b;
+  //   },
 
-    add: function (note) {
-      this.data.push(note);
-    },
+  //   add: function (note) {
+  //     this.data.push(note);
+  //   },
 
-    remove: function (note) {
-      for (var i = 0; i < this.data.length; i++) {
-        if (note === this.data[i]) {
-          var removed = this.data.splice(i, 1);
-          return removed;
-        }
-      }
-    },
+  //   remove: function (note) {
+  //     for (var i = 0; i < this.data.length; i++) {
+  //       if (note === this.data[i]) {
+  //         var removed = this.data.splice(i, 1);
+  //         return removed;
+  //       }
+  //     }
+  //   },
 
-    sort: function () {
-      // NOTE: data needs to be sorted every 100ms??
-      this.data.sort(this._cmp);
-    },
+  //   sort: function () {
+  //     // NOTE: data needs to be sorted every 100ms??
+  //     this.data.sort(this._cmp);
+  //   },
 
-    dump: function () {
-      var bucket = [];
-      for (var i = 0; i < this.data.length; i++) {
-        bucket.push(this.data[i].toString());
-      }
-      // console.log(bucket, this.data.length);
-      return bucket;
-    }
+  //   dump: function () {
+  //     var bucket = [];
+  //     for (var i = 0; i < this.data.length; i++) {
+  //       bucket.push(this.data[i].toString());
+  //     }
+  //     // console.log(bucket, this.data.length);
+  //     return bucket;
+  //   }
 
-  };
+  // };
 
 
   /**
@@ -362,97 +366,348 @@ var Timebase = (function (WX) {
    * - how does it perform on massive copy/paste
    */
 
-  function NoteList_Heap() {
-    this.data = [];
+  // function NoteList_Heap() {
+  //   this.data = [];
+  // }
+
+  // NoteList_Heap.prototype = {
+
+  //   _bubbleUp: function(idx) {
+  //     var self = this.data[idx];
+  //     while (idx > 0) {
+  //       var parentIdx = Math.floor((idx + 1) / 2) - 1,
+  //           parent = this.data[parentIdx];
+  //       // if start time are in order, break
+  //       if (self.start >= parent.start) break;
+  //       // otherwise swap parent and self
+  //       this.data[parentIdx] = self;
+  //       this.data[idx] = parent;
+  //       idx = parentIdx;
+  //     }
+  //   },
+
+  //   _sinkDown: function (idx) {
+  //     var len = this.data.length,
+  //         self = this.data[idx];
+  //     while (true) {
+  //       // get indices and vars
+  //       var child2Idx = (idx + 1) * 2,
+  //           child1Idx = child2Idx - 1,
+  //           child1, child2, swap = null;
+  //       // check child 1
+  //       if (child1Idx < len) {
+  //         child1 = this.data[child1Idx];
+  //         if (child1.start < self.start) {
+  //           swap = child1Idx;
+  //         }
+  //       }
+  //       // check child 2
+  //       if (child2Idx < len) {
+  //         child2 = this.data[child2Idx];
+  //         if (child2.start < (swap === null ? self.start : child1.start)) {
+  //           swap = child2Idx;
+  //         }
+  //       }
+  //       // no need to swap, finish
+  //       if (swap === null) break;
+  //       // other wise swap and continue
+  //       this.data[idx] = this.data[swap];
+  //       this.data[swap] = self;
+  //       idx = swap;
+  //     }
+  //   },
+
+  //   _pop: function () {
+  //     var first = this.data[0];
+  //     var end = this.data.pop();
+  //     if (this.data.length > 0) {
+  //       this.data[0] = end;
+  //       this._sinkDown(0);
+  //     }
+  //     return first;
+  //   },
+
+  //   add: function () {
+  //     for (var i = 0; i < arguments.length; i++) {
+  //       // add note to end, and bubble up
+  //       this.data.push(arguments[i]);
+  //       this._bubbleUp(this.data.length - 1);
+  //     }
+  //   },
+
+  //   remove: function (note) {
+  //     // search for note
+  //     for (var i = 0, l = this.data.length; i < l; i++) {
+  //       if (this.data[i] != note) continue;
+  //       // when found, pop a note at the end
+  //       var end = this.data.pop();
+  //       // if popped data is what we need to remove, stop
+  //       if (i === l - 1) break;
+  //       // otherwise, continue
+  //       this.data[i] = end;
+  //       this._bubbleUp(i);
+  //       this._sinkDown(i);
+  //       break;
+  //     }
+  //   }
+
+  // };
+
+
+  /**
+   * @class  Transport
+   * @description singleton, manages transport and master clocking
+   */
+
+  function Transport(BPM) {
+
+    this.BPM = BPM;
+    this.oldBPM = BPM;
+
+    // NOTES:
+    // - absolute time: time expressed in audioContext time
+    // - sec: linear time in seconds
+    // - tick: musical time, minimum unit of MBT timebase (varies on BPM)
+    // * if not specified in signature, it handles as tick (musical time)
+    // * however, all the internal calculation should be in seconds
+
+    // origin in absolute time and now reference
+    // absOrigin is origin of timeline, in terms of audioContext time;
+    // so absOrigin is '0' position of playhead in linear time
+    this.absOrigin = 0.0;
+    this.absOldNow = 0.0;
+
+    // time references, lookahead in seconds
+    this.now = 0.0;
+    this.loopStart = 0.0;
+    this.loopEnd = 0.0;
+    this.lookahead = 0.0;
+
+    // beats, ticks in seconds
+    this.BIS = 0.0;
+    this.TIS = 0.0;
+
+    // playback queue, connected notelists, views
+    this.playbackQ = [];
+    this.notelists = [];
+    this.views = [];
+
+    // switches
+    this.RUNNING = false;
+    this.LOOP = false;
+    this.USE_METRONOME = false;
+
+    // init BPM and initiate loop
+    this.setBPM(BPM);
+    this._loop();
   }
 
-  NoteList_Heap.prototype = {
+  Transport.prototype = {
 
-    _bubbleUp: function(idx) {
-      var self = this.data[idx];
-      while (idx > 0) {
-        var parentIdx = Math.floor((idx + 1) / 2) - 1,
-            parent = this.data[parentIdx];
-        // if start time are in order, break
-        if (self.start >= parent.start) break;
-        // otherwise swap parent and self
-        this.data[parentIdx] = self;
-        this.data[idx] = parent;
-        idx = parentIdx;
+    /**
+     * utilities
+     */
+
+    tick2sec: function (tick) {
+      return tick * this.TIS;
+    },
+
+    sec2tick: function (sec) {
+      return sec / this.TIS;
+    },
+
+    /**
+     * setter and getter
+     */
+
+    getAbsTimeInSec: function (tick) {
+      return this.absOrigin + this.tick2sec(tick);
+    },
+
+    getBPM: function () {
+      return this.BPM;
+    },
+
+    getNowInSec: function () {
+      return this.now;
+    },
+
+    getNow: function () {
+      return this.sec2tick(this.now);
+    },
+
+    getLoopDurationInSec: function() {
+      return this.loopEnd - this.loopStart;
+    },
+
+    getLoopDuration: function() {
+      return this.sec2tick(this.getLoopDurationInSec());
+    },
+
+    setBPM: function (BPM) {
+      this.BPM = BPM;
+      var factor = this.oldBPM / this.BPM;
+      this.oldBPM = this.BPM;
+      // recalcualte beat in seconds, tick in seconds
+      this.BIS = 60.0 / this.BPM;
+      this.TIS = this.BIS / TICKS_PER_BEAT;
+      // lookahead is 32 ticks (1/64th note)
+      this.lookahead = this.TIS * 32;
+      // update time references based on tempo change
+      this.now *= factor;
+      this.loopStart *= factor;
+      this.loopEnd *= factor;
+      this.absOrigin = WX.now - this.now;
+    },
+
+    setNowInSec: function (sec) {
+      // update now and absolute origin
+      this.now = sec;
+      this.absOrigin = WX.now - this.now;
+      // get tick of current linear now
+      var tick = this.sec2tick(this.now);
+      // reposition playhead for notelists at now
+      for (var i = 0; i < this.notelists.length; i++) {
+        this.notelists[i].setPlayheadAtTick(tick);
       }
     },
 
-    _sinkDown: function (idx) {
-      var len = this.data.length,
-          self = this.data[idx];
-      while (true) {
-        // get indices and vars
-        var child2Idx = (idx + 1) * 2,
-            child1Idx = child2Idx - 1,
-            child1, child2, swap = null;
-        // check child 1
-        if (child1Idx < len) {
-          child1 = this.data[child1Idx];
-          if (child1.start < self.start) {
-            swap = child1Idx;
+    setNow: function (tick) {
+      this.setNowInSec(this.tick2sec(tick));
+    },
+
+    setLoop: function (start, end) {
+      this.loopStart = this.tick2sec(start);
+      this.loopEnd = this.tick2sec(end);
+    },
+
+    /**
+     * NEED REDESIGN: scan and schedule
+     */
+
+    step: function () {
+      // only transport is in 'play'
+      if (this.RUNNING) {
+        // advancing, gets new absolute now
+        var absNow = WX.now;
+        this.now += (absNow - this.absOldNow);
+        this.absOldNow = absNow;
+
+        // scan notes and throw them into playbackQ
+        this.scanNotes();
+
+        // TODO: schedule scanned notes
+        // play(playbackQ)
+
+        // TODO: pulse metronome
+        // if (this.USE_METRONOME && this.metronome) {
+        //   // if nextPulse is in lookahead range, schedule it
+        //   this.metronome.play(this.now, this.lookahead);
+        // }
+
+        // ORIGINAL CODE:
+        // var tick = mtime2tick(this.nextClick);
+        // if (this.getLinearTime(tick) < this.ltime_now + this.lookahead) {
+        //   var accent = (this.nextClick.beat % 4 === 0) ? true : false;
+        //   this.metronome.play(this.getAbsoluteTime(tick), accent);
+        //   this.nextClick.beat += 1;
+        // }
+
+        this.flushPlaybackQ();
+
+        // handle looping
+        if (this.LOOP) {
+          if (this.loopEnd - (this.now + this.lookahead) < 0) {
+            this.setNowInSec(this.loopStart - (this.loopEnd - this.now));
           }
         }
-        // check child 2
-        if (child2Idx < len) {
-          child2 = this.data[child2Idx];
-          if (child2.start < (swap === null ? self.start : child1.start)) {
-            swap = child2Idx;
-          }
+      }
+    },
+
+    // scan notes in range and advance playhead in each notelist
+    scanNotes: function () {
+      for (var i = 0; i < this.notelists.length; i++) {
+        var end = this.sec2tick(this.now + this.lookahead);
+        var notes = this.notelists[i].scan(end);
+        // push notes into playbackQ
+        if (notes) {
+          Array.prototype.push.apply(this.playbackQ, notes);
         }
-        // no need to swap, finish
-        if (swap === null) break;
-        // other wise swap and continue
-        this.data[idx] = this.data[swap];
-        this.data[swap] = self;
-        idx = swap;
       }
     },
 
-    _pop: function () {
-      var first = this.data[0];
-      var end = this.data.pop();
-      if (this.data.length > 0) {
-        this.data[0] = end;
-        this._sinkDown(0);
-      }
-      return first;
-    },
-
-    add: function () {
-      for (var i = 0; i < arguments.length; i++) {
-        // add note to end, and bubble up
-        this.data.push(arguments[i]);
-        this._bubbleUp(this.data.length - 1);
+    setScanPosition: function (sec) {
+      for (var i = 0; i < this.notelists.length; i++) {
+        this.notelists[i].setPlayheadAtTick(this.sec2tick(sec));
       }
     },
 
-    remove: function (note) {
-      // search for note
-      for (var i = 0, l = this.data.length; i < l; i++) {
-        if (this.data[i] != note) continue;
-        // when found, pop a note at the end
-        var end = this.data.pop();
-        // if popped data is what we need to remove, stop
-        if (i === l - 1) break;
-        // otherwise, continue
-        this.data[i] = end;
-        this._bubbleUp(i);
-        this._sinkDown(i);
-        break;
+    // background loop
+    _loop: function () {
+      // advance when transport is running
+      this.step();
+      // update any linked view
+      this.updateView();
+      // next
+      requestAnimationFrame(this._loop.bind(this));
+    },
+
+    /**
+     * transport
+     */
+
+    isRunning: function () {
+      return this.RUNNING;
+    },
+
+    start: function () {
+      // flush queue and reset scanner position
+      this.playbackQ.length = 0;
+      this.setScanPosition(this.now);
+      // arrange time references
+      var absNow = WX.now;
+      this.absOrigin = absNow - this.now;
+      this.absOldNow = absNow;
+      // toggle switch
+      this.RUNNING = true;
+    },
+
+    pause: function () {
+      this.RUNNING = false;
+    },
+
+    rewind: function () {
+      this.setNowInSec(0.0);
+    },
+
+    /**
+     * notelists
+     */
+
+    addNoteList: function (notelist) {
+      this.notelists.push(notelist);
+    },
+
+    removeNoteList: function (notelist) {
+      //
+    },
+
+    flushPlaybackQ: function () {
+      this.playbackQ.length = 0;
+    },
+
+    /**
+     * update views
+     */
+    updateView: function (viewdata) {
+      // send data to update view and controller (polymer element)
+      for (var i = 0; i < this.views.length; i++) {
+        this.views[i].update(viewdata[i]);
       }
     }
 
   };
 
-
-  /**
-   * Timeline
-   */
 
 
 
@@ -475,7 +730,9 @@ var Timebase = (function (WX) {
 
     createNoteList: function () {
       return new NoteList();
-    }
+    },
+
+    Transport: new Transport(120)
 
   };
 
